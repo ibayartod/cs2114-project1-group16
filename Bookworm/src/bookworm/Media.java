118da3ade03860a7474d0fdcde8806e0c1747608
ibayartod <ibayartod@vt.edu>
2026-09-23 19:53:02 -0400

    media class

--- Bookworm/src/bookworm/Media.java
+++ Bookworm/src/bookworm/Media.java
@@ -1,28 +1,33 @@
 package bookworm;
 
-
 public class Media {
-    
+
     private String title;
-    
+
     private int id;
-    
+
     private boolean isAvailable;
-    
-    public Media() {
-        
+
+    public Media(String mTitle, int mId, boolean mIsAvailable) {
+        title = mTitle;
+        id = mId;
+        isAvailable = mIsAvailable;
     }
-    
+
+
     public String getName() {
-        return "";
+        return title;
     }
-    
+
+
     public boolean isAvailable() {
         return isAvailable;
     }
-    
+
+
     public void print() {
-        
+        System.out.println("Media title: " + title + ", Media Id: " + id
+            + ", Avaliability: " + isAvailable);
     }
 
 }
