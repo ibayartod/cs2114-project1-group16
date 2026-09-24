9cdf1cfc0ac62c43aa29c224d6724825753858cf
limd01 <donggheonl@vt.edu>
2026-09-24 19:05:13 -0400

    Update Movie.java

--- Bookworm/src/bookworm/Movie.java
+++ Bookworm/src/bookworm/Movie.java
@@ -1,6 +1,8 @@
 package bookworm;
 
-
+/**
+ * Represents a movie.
+ */
 public class Movie extends Media {
     
     private String director;
@@ -9,34 +11,66 @@ public class Movie extends Media {
     
     private String rating; // 1-5
     
+    /**
+     * Creates a movie.
+     */
     public Movie() {
-        
+        director = "";
+        runtime = "";
+        rating = "";
     }
     
+     /**
+     * Gets the director.
+     * @return director
+     */
     public String getDirector() {
         return director;
     }
     
+    /**
+     * Gets the runtime.
+     * @return runtime
+     */
     public String getRuntime() {
         return runtime;
     }
     
+    /**
+     * Gets the rating.
+     * @return rating
+     */
     public String getRating() {
         return rating;
     }
     
+    /**
+     * Sets the director.
+     * @param director new director
+     */
     public void setDirector(String director) {
         this.director = director
     }
-    
+
+    /**
+     * Sets the runtime.
+     * @param runtime new runtime
+     */
     public void setRuntime(String runtime) {
         this.runtime = runtime;
     }
-    
+
+    /**
+     * Sets the rating.
+     * @param rating new rating
+     */
     public void setRating(String rating) {
         this.rating = rating;
     }
-    
+
+    /**
+     * Prints movie information.
+     */
     @Override
     public void print() {
         System.out.println("Director" + director);
