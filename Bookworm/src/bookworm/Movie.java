0dc56d98816f4e79e1d2903443191b7727d8f86e
limd01 <donggheonl@vt.edu>
2026-09-23 20:19:36 -0400

    Update Movie.java

--- Bookworm/src/bookworm/Movie.java
+++ Bookworm/src/bookworm/Movie.java
@@ -13,9 +13,34 @@ public class Movie extends Media {
         
     }
     
+    public String getDirector() {
+        return director;
+    }
+    
+    public String getRuntime() {
+        return runtime;
+    }
+    
+    public String getRating() {
+        return rating;
+    }
+    
+    public String setDirector(String director) {
+        this.director = director
+    }
+    
+    public String setRuntime(String runtime) {
+        this.runtime = runtime;
+    }
+    
+    public String setRating(String rating) {
+        this.rating = rating;
+    }
+    
     @Override
     public void print() {
-        
+        System.out.println("Director" + director);
+        System.out.println("Runtime" + runtime);
+        System.out.println("Rating" + rating);
     }
-
 }
