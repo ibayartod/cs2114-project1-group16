924e62e15bea3706a3ae281fbbfd9b794dcf2fb1
Oliver Hardi <124931746+OliverHardi@users.noreply.github.com>
2026-09-23 20:11:51 -0400

    Implement user and media management methods

--- Bookworm/src/bookworm/Library.java
+++ Bookworm/src/bookworm/Library.java
@@ -2,53 +2,184 @@ package bookworm;
 
 import java.util.ArrayList;
 
+// -------------------------------------------------------------------------
+/**
+ *  The overarching Library class
+ * 
+ *  @author oliver
+ *  @version Sep 23, 2026
+ */
 public class Library {
         
     private ArrayList<User> users;
     
     private ArrayList<Media> media;
     
+    // ----------------------------------------------------------
+    /**
+     * Create a new Library object.
+     */
     public Library() {
         
     }
     
+    private int getUserIndex(User user) {
+        return this.users.indexOf(user);
+    }
+    
+    private int getMediaIndex(Media m) {
+        return this.media.indexOf(m);
+    }
+    
     // add/remove
     
-    public boolean addUser() {
-        return false;
+    // ----------------------------------------------------------
+    /**
+     * Adds a user to the list
+     * @param user The user to be added
+     * @return true if it succeeds, false if it can't add the user
+     */
+    public boolean addUser(User user) {
+        if (user == null) {
+            return false;
+        }
+        users.add(user);
+        return true;
     }
     
-    public boolean removeUser() {
-        return false;
+    // ----------------------------------------------------------
+    /**
+     * Removes a user from the list of users
+     * @param user the user to be removed
+     * @return the user that was removed, or null if it doesn't exist
+     */
+    public User removeUser(User user) {
+        int index = getUserIndex(user);
+        if (index == -1) {
+            return null;
+        }
+        return users.remove(index);
     }
     
-    public boolean addMedia() {
-        return false;
+    // ----------------------------------------------------------
+    /**
+     * Adds a media to the media
+     * @param m The media to be added
+     * @return true if adding succeeds, false if it fails
+     */
+    public boolean addMedia(Media m) {
+        if (m == null) {
+            return false;
+        }
+        media.add(m);
+        return true;
     }
     
-    public boolean removeMedia() {
-        return false;
+    // ----------------------------------------------------------
+    /**
+     * Removes media from the media list
+     * @param m the media to be removed
+     * @return the media that was removed, null if it doesn't exist
+     */
+    public Media removeMedia(Media m) {
+        int index = getMediaIndex(m);
+        if (index == -1) {
+            return null;
+        }
+        return media.remove(index);
     }
     
     // getters/setters
     
+    // ----------------------------------------------------------
+    /**
+     * Gets the user by searching their username (ignoring case)
+     * @param str the input string
+     * @return the user that was searched for, or null if it fails
+     */
     public User getUser(String str) {
-        return new User();
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getUsername().equalsIgnoreCase(str)) {
+                return users.get(i);
+            }
+        }
+        return null;
     }
     
+    // ----------------------------------------------------------
+    /**
+     * Gets the user from searching the id
+     * more precise than the string version because every user has unique ids
+     * @param id the input id
+     * @return the user that was searched for
+     */
     public User getUser(int id) {
-        return new User();
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getId() == id) {
+                return users.get(i);
+            }
+        }
+        return null;
     }
     
     public int getIdOfUser(String name) {
-        return -1;
+        User user = getUser(name);
+        if (user == null) {
+            return -1;
+        }
+        return user.getId();
     }
     
     
     // other methods
     
-    public boolean checkOutMedia() {
-        return false;
+    // ----------------------------------------------------------
+    /**
+     * Checks out media from the library and gives it to a user
+     * @param user the user checking it out
+     * @param m the media being checked out
+     * @return true upon succeed, else false
+     */
+    public boolean checkOutMedia(User user, Media m) {
+        if (user == null || m == null) {
+            return false;
+        }
+        
+        if (!m.isAvailable()) {
+            return false;
+        }
+        
+        user.checkOut(m);
+        
+        m.setAvailable(false);
+        
+        return true;
+    }
+    
+    // ----------------------------------------------------------
+    /**
+     * Returns media from a user and makes it available to check out again
+     * @param user the user returning the media
+     * @param m the media being checked in
+     * @return true upon succeed, else false
+     */
+    public boolean returnMedia(User user, Media m) {
+        
+        if (user == null || media == null) {
+            return false;
+        }
+        
+        if (m.isAvailable()) {
+            return false; // it's already checked back in
+        }
+        
+        if (!user.checkIn(m)) {
+            return false; // the user doesn't have the media
+        }
+        
+        m.setAvailable(true);
+        
+        return true;
     }
     
     public Media search(String str) {
