6b38463c0f55e1eba4879762d9eed4e03e4a81e4
limd01 <donggheonl@vt.edu>
2026-09-24 19:07:57 -0400

    Update MovieTest.java

--- Bookworm/src/bookworm/MovieTest.java
+++ Bookworm/src/bookworm/MovieTest.java
@@ -2,30 +2,48 @@ package bookworm;
 
 import student.TestCase;
 
+/**
+ * Tests the Movie class.
+ */
 public class MovieTest extends TestCase {
 
     private Movie movie;
 
+    /**
+     * Sets up the test.
+     */
     public void setUp() {
         movie = new Movie();
     }
 
+    /**
+     * Tests the constructor.
+     */
     public void testConstructor() {
         assertEquals("", movie.getDirector());
         assertEquals("", movie.getRuntime());
         assertEquals("", movie.getRating());
     }
 
+    /**
+     * Tests setDirector.
+     */
     public void testSetDirector() {
         movie.setDirector("Christopher Nolan");
         assertEquals("Christopher Nolan", movie.getDirector());
     }
 
+     /**
+     * Tests setRuntime.
+     */
     public void testSetRuntime() {
         movie.setRuntime("148");
         assertEquals("148", movie.getRuntime());
     }
 
+    /**
+     * Tests setRating.
+     */
     public void testSetRating() {
         movie.setRating("5");
         assertEquals("5", movie.getRating());
