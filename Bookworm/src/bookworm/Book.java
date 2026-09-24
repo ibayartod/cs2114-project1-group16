15692152f16b81e26f4cf22bcb1041c32fe85c08
limd01 <donggheonl@vt.edu>
2026-09-23 20:18:05 -0400

    Update Book.java

--- Bookworm/src/bookworm/Book.java
+++ Bookworm/src/bookworm/Book.java
@@ -13,9 +13,41 @@ public class Book extends Media {
         
     }
     
+    public void print() {
+        author = "";
+        genre = "";
+        ISBN =  "";
+    }
+    
+    public String getAuthor() {
+        return author;
+    }
+    
+    public String getGenre() {
+        return genre;
+    }
+    
+    public String getISBN() {
+        return ISBN;
+    }
+    
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    
+    public void setISBN() {
+        this.ISBN = ISBN;
+    }
+    
     @Override
     public void print() {
-        
+        System.out.println("Author: " + author);
+        System.out.println("Genre: " + genre);
+        System.out.println("ISBN" + ISBN);
     }
-
+ 
 }
